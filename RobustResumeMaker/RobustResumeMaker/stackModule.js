56a875adf48baea83f3014224ffe7ec479c0673d
debarron <de.lopezbarron@gmail.com>
2016-11-12 19:15:09 -0600

    Using StackExchange API in NodeJS

--- RobustResumeMaker/RobustResumeMaker/stackModule.js
+++
@@ -1,6 +0,0 @@
-// stackModule.js
-
-"client_id": 8363,
-"client_secret": "bp7HSAqTpCwV9CLUKIfXAg(("
-"key": "QZM9sJ9hrP4Fj1a3GF4FzQ(("
-"redirect_uri": "https://stackexchange.com/oauth/login_success"
